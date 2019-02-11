2051d7b119ee55b47a59fb60ab2e9a7a2ec65dc8
Uikas <GRA17475@spengergasse.at>
2019-02-11 11:35:11 +0100

    Man kauf dir ein Laptop

--- src/at/spengergasse/Main.java
+++ src/at/spengergasse/Main.java
@@ -24,29 +24,168 @@ public class Main {
                 System.out.println("Bitte geben sie eine Druckstärke ein:");
                 String eingabedruck =sc.nextLine();
                 double hgs =Double.parseDouble(eingabedruck);
-                if(hgs < 21.6){
+                if(hgs <= 21.6){
                     System.out.println("Passen sie auf, sie sind in Lebensgefahr");
+                }
+                else{
+                    System.out.println("Sie sind gesund");
                 }
             }
             if( height ==155 ||height ==156||height ==157||height ==158||height ==159) {
                 System.out.println("Bitte geben sie eine Druckstärke ein:");
                 String eingabedruck = sc.nextLine();
                 double hgs = Double.parseDouble(eingabedruck);
-                if (hgs < 22.9) {
+                if (hgs <= 22.9) {
                     System.out.println("Passen sie auf, sie sind in Lebensgefahr");
+                }
+                else{
+                    System.out.println("Sie sind gesund");
                 }
             }
             if( height ==160 ||height ==161||height ==162||height ==163||height ==164) {
                 System.out.println("Bitte geben sie eine Druckstärke ein:");
                 String eingabedruck = sc.nextLine();
                 double hgs = Double.parseDouble(eingabedruck);
-                if (hgs < 24.0) {
+                if (hgs <= 24.0) {
                     System.out.println("Passen sie auf, sie sind in Lebensgefahr");
                 }
+                else{
+                    System.out.println("Sie sind gesund");
+                }
             }
+            if( height ==165 ||height ==166||height ==167||height ==168||height ==169) {
+                System.out.println("Bitte geben sie eine Druckstärke ein:");
+                String eingabedruck = sc.nextLine();
+                double hgs = Double.parseDouble(eingabedruck);
+                if (hgs <= 25.0) {
+                    System.out.println("Passen sie auf, sie sind in Lebensgefahr");
+                }
+                else{
+                    System.out.println("Sie sind gesund");
+                }
+            }
+            if( height ==170 ||height ==171||height ==172||height ==173||height ==174) {
+                System.out.println("Bitte geben sie eine Druckstärke ein:");
+                String eingabedruck = sc.nextLine();
+                double hgs = Double.parseDouble(eingabedruck);
+                if (hgs <= 26.0) {
+                    System.out.println("Passen sie auf, sie sind in Lebensgefahr");
+                }
+                else{
+                    System.out.println("Sie sind gesund");
+                }
+            }
+            if( height ==175 ||height ==176||height ==177||height ==178||height ==179) {
+                System.out.println("Bitte geben sie eine Druckstärke ein:");
+                String eingabedruck = sc.nextLine();
+                double hgs = Double.parseDouble(eingabedruck);
+                if (hgs <= 26.7) {
+                    System.out.println("Passen sie auf, sie sind in Lebensgefahr");
+                }
+                else{
+                    System.out.println("Sie sind gesund");
+                }
+            }
+            if( height ==180 ||height ==181||height ==182||height ==183||height ==184) {
+                System.out.println("Bitte geben sie eine Druckstärke ein:");
+                String eingabedruck = sc.nextLine();
+                double hgs = Double.parseDouble(eingabedruck);
+                if (hgs <= 27.6) {
+                    System.out.println("Passen sie auf, sie sind in Lebensgefahr");
+                }
+                else{
+                    System.out.println("Sie sind gesund");
+                }
+            }
+
+
+        }
+        if(alter ==20 || alter == 21 || alter == 22 || alter ==23 ||alter ==24){
+            System.out.println("Bitte geben sie eine Größe zwischen 150 und 184:");
+            String eingabeheight =sc.nextLine();
+            int height =parseInt(eingabeheight);
+            if( height ==150 ||height ==151||height ==152||height ==153||height ==154){
+                System.out.println("Bitte geben sie eine Druckstärke ein:");
+                String eingabedruck =sc.nextLine();
+                double hgs =Double.parseDouble(eingabedruck);
+                if(hgs <= 23.7){
+                    System.out.println("Passen sie auf, sie sind in Lebensgefahr");
+                }
+                else{
+                    System.out.println("Sie sind gesund");
+                }
+            }
+            if( height ==155 ||height ==156||height ==157||height ==158||height ==159) {
+                System.out.println("Bitte geben sie eine Druckstärke ein:");
+                String eingabedruck = sc.nextLine();
+                double hgs = Double.parseDouble(eingabedruck);
+                if (hgs <= 24.8) {
+                    System.out.println("Passen sie auf, sie sind in Lebensgefahr");
+                }
+                else{
+                    System.out.println("Sie sind gesund");
+                }
+            }
+            if( height ==160 ||height ==161||height ==162||height ==163||height ==164) {
+                System.out.println("Bitte geben sie eine Druckstärke ein:");
+                String eingabedruck = sc.nextLine();
+                double hgs = Double.parseDouble(eingabedruck);
+                if (hgs <= 26.1) {
+                    System.out.println("Passen sie auf, sie sind in Lebensgefahr");
+                }
+                else{
+                    System.out.println("Sie sind gesund");
+                }
+            }
+            if( height ==165 ||height ==166||height ==167||height ==168||height ==169) {
+                System.out.println("Bitte geben sie eine Druckstärke ein:");
+                String eingabedruck = sc.nextLine();
+                double hgs = Double.parseDouble(eingabedruck);
+                if (hgs <= 27.1) {
+                    System.out.println("Passen sie auf, sie sind in Lebensgefahr");
+                }
+                else{
+                    System.out.println("Sie sind gesund");
+                }
+            }
+            if( height ==170 ||height ==171||height ==172||height ==173||height ==174) {
+                System.out.println("Bitte geben sie eine Druckstärke ein:");
+                String eingabedruck = sc.nextLine();
+                double hgs = Double.parseDouble(eingabedruck);
+                if (hgs <= 28.0) {
+                    System.out.println("Passen sie auf, sie sind in Lebensgefahr");
+                }
+                else{
+                    System.out.println("Sie sind gesund");
+                }
+            }
+            if( height ==175 ||height ==176||height ==177||height ==178||height ==179) {
+                System.out.println("Bitte geben sie eine Druckstärke ein:");
+                String eingabedruck = sc.nextLine();
+                double hgs = Double.parseDouble(eingabedruck);
+                if (hgs <= 29.0) {
+                    System.out.println("Passen sie auf, sie sind in Lebensgefahr");
+                }
+                else{
+                    System.out.println("Sie sind gesund");
+                }
+            }
+            if( height ==180 ||height ==181||height ==182||height ==183||height ==184) {
+                System.out.println("Bitte geben sie eine Druckstärke ein:");
+                String eingabedruck = sc.nextLine();
+                double hgs = Double.parseDouble(eingabedruck);
+                if (hgs <= 29.6) {
+                    System.out.println("Passen sie auf, sie sind in Lebensgefahr");
+                }
+                else{
+                    System.out.println("Sie sind gesund");
+                }
+            }
+
+
         }
         else{
-            System.out.println("Bitte ein Alter das zugelassen ist");
+            System.out.println("Bitte ein  zugelassen Wert eingeben");
         }
 
 
